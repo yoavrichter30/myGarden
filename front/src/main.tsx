7e54b4ba62cdaa8db00a5d62e306b4093740753e
nir <nir.dancig1@gmail.com>
2024-01-12 12:33:15 +0200

    project structure

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import SignIn from './pages/sign-in/SignIn.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
 )
---
+++ front/src/pages/sign-in/SignIn.tsx
@@ -0,0 +1,11 @@
+
+function SignIn() {
+  return (
+    <>
+
+      <h1>Signin</h1>
+    </>
+  )
+}
+
+export default SignIn
