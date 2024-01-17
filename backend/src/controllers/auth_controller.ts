30811bf7ab9f0ae704f57358695156ab140c95e1
Yoav Richter <richter.yoav30@gmail.com>
2024-01-18 00:37:40 +0200

    finish login method

--- backend/src/controllers/auth_controller.ts
+++ backend/src/controllers/auth_controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import UserSchema from '../models/user';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const registry = async (request: Request, response: Response) => {
     const username = request.body.username;
@@ -30,5 +31,42 @@ const registry = async (request: Request, response: Response) => {
     } catch (err) {
         response.status(500).send(`Error in creating new user - ${username}, \n ${err}`);
     }
+}
 
+const login = async (request: Request, response: Response) => {
+    const email = request.body.email;
+    const password = request.body.password;
+
+    if (!email || !password){
+        return response.status(400).send("Missing parameters");
+    }
+
+    const user = await UserSchema.findOne({'email': email});
+    if (user == null) {
+        return response.status(404).send("User for that email doesn't exists");
+    } 
+
+    const isPasswdsMatches = await bcrypt.compare(password, user.password);
+    if (!isPasswdsMatches){
+        return response.status(401).send("Wrong email or password");
+    }
+
+    const accessToken = jwt.sign({_id: user._id}, process.env.JWT_SECRET as string, { expiresIn: process.env.JWT_EXPIRATION });
+    const refreshToken = jwt.sign({ _id: user._id }, process.env.JWT_REFRESH_SECRET as string);
+    if (!user.refreshTokens) {
+        user.refreshTokens = [refreshToken];
+    } else {
+        user.refreshTokens.push(refreshToken);
+    }
+    
+    try {
+        await user.save();
+
+        return response.status(200).send({
+            'accessToken': accessToken,
+            'refreshToken': refreshToken
+        });
+    } catch (err) {
+        response.status(500).send(`Error login for - ${user.username}, \n ${err}`);
+    }
 }
