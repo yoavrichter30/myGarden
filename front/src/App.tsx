e8ef45fa9b4861a51f2242343ef980232be9ca3e
nir <nir.dancig1@gmail.com>
2024-01-26 11:19:03 +0200

    navbar and routing to home page (without private routing)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     <>
     <ThemeProvider theme={baseTheme}>
       <PublicLayout />
-
+      
       <CssBaseline />
 
       <Outlet />
--- front/src/layout/PublicLayout.tsx
+++ front/src/layout/PublicLayout.tsx
@@ -1,16 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import SearchAppBar from "./Appbar/Appbar";
-import { StyledEngineProvider } from '@mui/material/styles';
 
 export default function PublicLayout() {
     return (    
       <>
       <SearchAppBar />
-{/*   
-      <CssBaseline />
-  
-      <ThemeProvider theme={baseTheme}>
-        <Outlet />
-      </ThemeProvider> */}
+        <Routes>
+          {/* <Route path="/home" element={<SignIn />} />
+          <Route path="/profile" element={<SignUp />} /> */}
+        </Routes>
       </>
     )
   }
