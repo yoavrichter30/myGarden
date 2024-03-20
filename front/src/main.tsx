883e8c0c11b96714c399d41b0592054e0ac7ac78
Yoav Richter <richter.yoav30@gmail.com>
2024-03-20 09:32:32 +0200

    hash router

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import SignIn from './pages/sign-in/SignIn.tsx';
 import SignUp from './pages/sign-up/SignUp.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App/>   
-      </BrowserRouter>
+      </HashRouter>
    </React.StrictMode>,
 )
