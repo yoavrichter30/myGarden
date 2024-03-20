6ecad1ee446894890d0f4dc00a12fa51aaab9204
Yoav Richter <richter.yoav30@gmail.com>
2024-03-20 10:44:30 +0200

    change base decleration

--- backend/src/routes/file.route.ts
+++ backend/src/routes/file.route.ts
@@ -2,7 +2,6 @@ import express from "express";
 import multer from "multer";
 
 // const base = "http://" + process.env.DOMAIN_BASE + ":" + process.env.PORT + "/";
-const base = process.env.NODE_ENV !== 'PRODUCTION' ? "http://localhost:8080/" : "https://10.10.248.182:443/";
 const filesLocation = process.env.NODE_ENV !== 'PRODUCTION' ? 'public/' : '../public'
 const router = express.Router();
 
@@ -21,6 +20,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.post('/', upload.single("file"), function (req, res) {
+    const base = process.env.NODE_ENV !== 'PRODUCTION' ? "http://localhost:8080/" : "https://10.10.248.182:443/";
     console.log(base + req.file!.path)
     res.status(200).send({ url: base + req.file!.path })
 });
